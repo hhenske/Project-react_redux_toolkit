78848f834812d4e23d4a8e6019adc799dfec4a08
Holly Henske <hcover333@gmail.com>
2025-06-02 13:31:44 -0500

    odered to show newest post on top

--- 01_lesson/src/features/posts/PostsList.js
+++ 01_lesson/src/features/posts/PostsList.js
@@ -5,8 +5,9 @@ import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-    const renderedPosts = posts.map(post => (
+    const renderedPosts = orderedPosts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
